975dc814ba34fca397b277f74b23938e184d142e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-02 22:33:56 -0500

    feat(main): added main view and behavior.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TranslateService} from "@ngx-translate/core";
+import {SideNavigationBarComponent} from "./public/components/side-navigation-bar/side-navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideNavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'catch-up';
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 }
