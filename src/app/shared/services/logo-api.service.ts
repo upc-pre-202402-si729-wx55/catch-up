f87e768909ea3d59c72476bb676f9f4a57b43bf4
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-02 21:02:51 -0500

    feat(logo): added clearbit logo api service client.

---
+++ src/app/shared/services/logo-api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoApiService {
+  baseUrl = 'https://logo.clearbit.com/';
+
+  constructor() { }
+
+  getUrlToLogo(source: any) {
+    return `${this.baseUrl}${ new URL(source.url).hostname}`;
+  }
+}
