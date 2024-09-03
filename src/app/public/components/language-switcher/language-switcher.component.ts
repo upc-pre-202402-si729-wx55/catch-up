652fda76be6d1d6bc627be4b1464ca87f35d0d4c
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-02 22:06:47 -0500

    feat(public): added language switcher component.

---
+++ src/app/public/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-language-switcher',
+  standalone: true,
+  imports: [
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    NgForOf
+  ],
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.css'
+})
+export class LanguageSwitcherComponent {
+  currentLang = 'en';
+  languages = ['en', 'es'];
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = translate.currentLang;
+  }
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
+
+}
