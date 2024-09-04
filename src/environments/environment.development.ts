7fbc7ef73c2d9b4b2aca573c16a93545dd396909
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-04 11:27:57 -0500

    feat(environment): added environment variable support.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+// Environment: development
+// This is the development environment configuration file.
+// Note: In real scenarios, this file is not commited to the repository.
+export const environment = {
+  production: false,
+  newsApiKey: 'fecf4feeffa64e4da682e7d268612ce5'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+// Environment: production
+// This is the production environment configuration file.
+// Note: In real scenarios, this file is not commited to the repository.
+export const environment = {
+  production: true,
+  newsApiKey: 'fecf4feeffa64e4da682e7d268612ce5'
+};
